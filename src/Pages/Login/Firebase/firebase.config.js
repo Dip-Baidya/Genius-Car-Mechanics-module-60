8ce0a6e416c2bbf7944fdac54ae39b70aedf005c
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-03-02 02:16:19 +0600

    setup Firebase

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAiNMizgiVXuIaV2_rMtVPy2Pkm6lips8E",
+    authDomain: "genius-car-mechanics-d22c6.firebaseapp.com",
+    projectId: "genius-car-mechanics-d22c6",
+    storageBucket: "genius-car-mechanics-d22c6.appspot.com",
+    messagingSenderId: "817983760798",
+    appId: "1:817983760798:web:be9e2e1fbd2db12e9e4e8f"
+};
+
+export default firebaseConfig;
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
+            <h2>Please Log In</h2>
             <button className="btn btn-warning">Google Sign In</button>
         </div>
     );
